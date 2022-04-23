36c827009583562da722e03e16ed56c6c87cc271
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-04-23 20:27:39 +0600

    steps added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-;const app = express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // use middleware
@@ -14,13 +14,21 @@ app.use(express.json());
 
 const uri = "mongodb+srv://dbuser1:v1Lv4PCTOmu6zfD2@cluster0.pta9f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db connected');
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("users");
+        const user = {name: 'Mehruz Saif Arnob', email: 'mehruzsaif@gmail.com'};
+        const result = await userCollection.insertOne(user);
+        console.log(`User inserted with id: ${result.insertedId}`);
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running Node CRUD Server')
